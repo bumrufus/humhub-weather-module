37e48d5a951ecb82239b78da0bd1fbcffff71094
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-05-27 05:06:30 +0900

    Update weatherframe.php

--- widgets/views/weatherframe.php
+++ widgets/views/weatherframe.php
@@ -15,7 +15,7 @@ use humhub\models\Setting;
   <div class="panel-body">
 
 <?= Html::beginTag('div') ?>
-<a class="weatherwidget-io" href="<?= $weatherUrl ?>" data-label_1="NIIGATA PREFECTURE" data-label_2="WEATHER" data-font="Roboto" data-icons="Climacons Animated" data-theme="weather_one" >NIIGATA PREFECTURE WEATHER</a>
+<a class="weatherwidget-io" href="<?= $weatherUrl ?>" data-label_2="WEATHER" data-font="Roboto" data-icons="Climacons Animated" data-theme="weather_one" ></a>
 <script>
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 </script>
